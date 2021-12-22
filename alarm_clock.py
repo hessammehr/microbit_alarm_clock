6e283b97a719aac1dfa2917c41afb37210f443cb
Hessam Mehr <hessammehr@gmail.com>
2021-12-22 18:31:05 +0000

    Alarm clock functional.

--- alarm_clock.py
+++ alarm_clock.py
@@ -1,113 +1,112 @@
-def binary_minutes():
-    pass
-def tick():
-    global seconds, minutes, hours
-    seconds += 1
-    if seconds == 60:
-        seconds = 0
-        minutes += 1
-    if minutes == 60:
-        minutes = 0
-        hours += 1
-    if minutes == 60:
-        minutes = 0
-        hours += 1
-    if hours == 24:
-        hours = 0
-def display_time():
-    if display_mode == 0:
-        display_binary()
-    else:
-        display_decimal()
-def display_binary():
-    binary_hours()
-    binary_minutes()
-    binary_seconds()
+from microbit import *
+import music
 
-def on_logo_long_pressed():
-    global mode
-    mode = 1
-input.on_logo_event(TouchButtonEvent.LONG_PRESSED, on_logo_long_pressed)
+ORIGIN = 0
+ALARM_ORIGIN = 0
+MODE = 'run'
 
-def get_brightness():
-    if brightness_mode == 0:
-        return input.light_level()
-    else:
-        return 255
+def time(milliseconds):
+    seconds = milliseconds // 1000
+    return (
+        seconds // 3600 % 24,
+        (seconds // 60) % 60,
+        seconds % 60
+    )
 
-def on_button_pressed_a():
-    global display_mode
-    if mode == 0:
-        if display_mode == 0:
-            display_mode = 1
+def show(num, y_offset, num_pixels, max_value):
+    brightness_levels = max_value // num_pixels
+    whole = num // brightness_levels
+    part = int((num / brightness_levels - whole) * 9)
+    for index in range(max_value // brightness_levels):
+        x, y = index % 5, y_offset + index // 5
+        if index < whole:
+            display.set_pixel(x, y, 9)
+        elif index == whole:
+            display.set_pixel(x, y, part)
         else:
-            display_mode = 0
-input.on_button_pressed(Button.A, on_button_pressed_a)
+            display.set_pixel(x, y, 0)
 
-def binary_seconds():
-    for index in range(11):
-        led.plot_brightness(index % 5,
-            index / 5,
-            Math.map(Math.constrain(seconds - index * 6, 0, 0),
-                0,
-                6,
-                0,
-                get_brightness()))
+def show_time(h, m, s):
+    show(s, 0, 5, 60)
+    show(m, 1, 10, 60)
+    h = '{:05b}'.format(h)
+    for i in range(5):
+        if h[i] == '0':
+            display.set_pixel(i, 4, 0)
+        else:
+            display.set_pixel(i, 4, 9)
+
 def play_alarm():
-    for index2 in range(4):
-        music.play_tone(262, music.beat(BeatFraction.WHOLE))
-        music.play_tone(220, music.beat(BeatFraction.WHOLE))
-        music.play_tone(262, music.beat(BeatFraction.WHOLE))
-        music.play_tone(220, music.beat(BeatFraction.WHOLE))
-        music.play_melody("C5 B A C5 B C5 C5 C5 ", 500)
-        for index3 in range(2):
-            music.change_tempo_by(100)
-            music.play_tone(262, music.beat(BeatFraction.WHOLE))
-            music.play_tone(220, music.beat(BeatFraction.WHOLE))
-            music.play_tone(262, music.beat(BeatFraction.WHOLE))
-            music.play_tone(220, music.beat(BeatFraction.WHOLE))
-            music.play_melody("C5 B A C5 B C5 C5 C5 ", 400)
-            music.play_tone(523, music.beat(BeatFraction.BREVE))
-        for index4 in range(4):
-            music.play_tone(880, music.beat(BeatFraction.DOUBLE))
-            music.play_tone(262, music.beat(BeatFraction.WHOLE))
-            music.play_tone(220, music.beat(BeatFraction.WHOLE))
-            music.play_tone(262, music.beat(BeatFraction.WHOLE))
-            music.play_tone(220, music.beat(BeatFraction.WHOLE))
-            music.play_melody("C5 B A C5 B C5 C5 C5 ", 500)
+    tempo = 120
+    for _ in range(4):
+        music.set_tempo(bpm=500)
+        music.pitch(262, 1 * 60000 // tempo)
+        music.pitch(220, 1 * 60000 // tempo)
+        music.pitch(262, 1 * 60000 // tempo)
+        music.pitch(220, 1 * 60000 // tempo)
+        music.play('C5 B A C5 B C5 C5 C5 '.split())
+        for _ in range(2):
+            tempo += 100
+            music.pitch(262, 1 * 60000 // tempo)
+            music.pitch(220, 1 * 60000 // tempo)
+            music.pitch(262, 1 * 60000 // tempo)
+            music.pitch(220, 1 * 60000 // tempo)
+            music.set_tempo(bpm=400)
+            music.play('C5 B A C5 B C5 C5 C5 '.split())
+            music.pitch(523, 4 * 60000 // tempo)
+        for _ in range(4):
+            music.pitch(880, 2 * 60000 // tempo)
+            music.pitch(262, 1 * 60000 // tempo)
+            music.pitch(220, 1 * 60000 // tempo)
+            music.pitch(262, 1 * 60000 // tempo)
+            music.pitch(220, 1 * 60000 // tempo)
+            music.set_tempo(bpm=500)
+            music.play('C5 B A C5 B C5 C5 C5 '.split())
+        if button_a.is_pressed() or button_b.is_pressed():
+            break
+    
+alarm_armed = False
+last_alarm = running_time()
 
-def on_button_pressed_ab():
-    global mode
-    mode = 2
-input.on_button_pressed(Button.AB, on_button_pressed_ab)
-
-def on_gesture_shake():
-    global brightness_mode
-    if brightness_mode == 0:
-        brightness_mode = 1
-    else:
-        brightness_mode = 0
-input.on_gesture(Gesture.SHAKE, on_gesture_shake)
-
-def binary_hours():
-    pass
-def display_decimal():
-    pass
-brightness_mode = 0
-display_mode = 0
-mode = 0
-hours = 0
-minutes = 0
-seconds = 0
-seconds = 0
-minutes = 0
-hours = 0
-mode = 0
-play_alarm()
-
-def on_forever():
-    if mode == 0:
-        basic.pause(1000)
-        tick()
-    display_time()
-basic.forever(on_forever)
+while True:
+    H, M, S = time(ORIGIN)
+    if pin_logo.is_touched():
+        if MODE == 'run':
+            MODE = 'adjust'
+        elif MODE == 'adjust':
+            MODE = 'adjust-alarm'
+        else:
+            MODE = 'run'
+        display.scroll(MODE, delay=50)
+    if MODE == 'adjust':
+        if button_a.is_pressed():
+            ORIGIN += 60000
+        elif button_b.is_pressed():
+            ORIGIN -= 60000
+        sleep(100)
+    if MODE == 'adjust-alarm':
+        alarm_armed = True
+        if accelerometer.was_gesture('shake'):
+            display.scroll('ALARM CLEARED')
+            alarm_armed = False
+            MODE = 'run'
+        if button_a.is_pressed():
+            ALARM_ORIGIN += 60000
+        elif button_b.is_pressed():
+            ALARM_ORIGIN -= 60000
+        show_time(*time(ALARM_ORIGIN))
+        sleep(100)
+        continue
+    h, m, s = time(running_time())
+    h += H
+    m += M
+    print(alarm_armed)
+    print(h, m, *time(ALARM_ORIGIN)[:2])
+    if (h, m) == time(ALARM_ORIGIN)[:2] and alarm_armed:
+        if -60000 < running_time() - last_alarm <  60000:
+            pass
+        else:
+            print('ALARM')
+            play_alarm()
+            last_alarm = running_time()
+    show_time(h, m, s)
